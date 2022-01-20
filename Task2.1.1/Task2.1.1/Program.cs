ab19e3321d0f2015f01b3939c3a96ca6a112bc29
Kirill Kovardakov <96287721+KirillKovardakov@users.noreply.github.com>
2022-01-20 20:27:30 +0400

    Added task 2.1.1

---
+++ Task2.1.1/Task2.1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MyString;
+
+namespace Task2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = (Console.ReadLine());
+            MyString(str);
+
+        }
+
+        private static void MyString(string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

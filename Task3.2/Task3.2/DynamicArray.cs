4d8b1264b5b4fc28241927c27ca6f1becf25c911
Kirill Kovardakov <96287721+KirillKovardakov@users.noreply.github.com>
2022-02-07 20:55:47 +0400

    Added task 3.2

---
+++ Task3.2/Task3.2/DynamicArray.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicArray
+{
+    public class DynamicArray<T> : IEnumerable<T>
+    {
+        public T[] Mass { get; set; } = new T[8];
+        private int Count { get; set; } = 0;
+        public DynamicArray()
+        {
+
+        }
+        public DynamicArray(int n)
+        {
+            Mass = new T[n];
+        }
+        public DynamicArray(IEnumerable<T> tempArray)
+        {
+            FactorCapacity(tempArray);
+            foreach (var item in tempArray)
+            {
+                if (item != null) Mass[Count] = item;
+                Count++;
+            }
+        }
+        public void Add(T temp)
+        {
+            Count++;
+            CapacityTest();
+            Mass[Count - 1] = temp;
+        }
+        public void AddRange(IEnumerable<T> temp)
+        {
+            FactorCapacity(temp);
+            foreach (var item in temp)
+            {
+                Count++;
+                Mass[Count - 1] = item;
+            }
+        }
+        private void CapacityTest()
+        {
+            if (Count > Mass.Length)
+            {
+                this.Mass = DoubleCapacity();
+            }
+        }
+        private T[] DoubleCapacity()
+        {
+            var newMass = new T[Mass.Length * 2];
+            for (int i = 0; i < Mass.Length; i++)
+            {
+                newMass[i] = Mass[i];
+            }
+            return newMass;
+        }
+        private void FactorCapacity(IEnumerable<T> temp)
+        {
+            int countAddArray = 0;
+            foreach (var item in temp)
+            {
+                countAddArray++;
+            }
+            int factor = 1;
+            while (countAddArray + Count > factor * Mass.Length) factor *= 2;
+            var newMass = new T[Mass.Length * factor];
+            for (int i = 0; i < Mass.Length; i++)
+            {
+                newMass[i] = Mass[i];
+            }
+            Mass = newMass;
+        }
+        public bool Insert(int position, T value)
+        {
+            if (position < 0 || position >= Count) throw new ArgumentOutOfRangeException();
+            ++Count;
+            CapacityTest();
+            for (int i = Count - 1; i > position; i--) //сдвиг элементов вправа
+            {
+                Mass[i] = Mass[i - 1];
+            }
+
+            Mass[position] = value;
+            return true;
+        }
+        public bool Remove(int position)
+        {
+            if (position < 0 || position >= Count) return false;
+            for (int i = position; i < Count- 1; i++)
+            {
+                Mass[i] = Mass[i + 1];
+            }
+            --Count;
+            return true;
+        }
+        public int Length() => Count;
+        public int Capacity() => Mass.Length;
+
+        public DynamicArray<T> GetEnumerator()
+        {
+            return new DynamicArray<T>(Mass);
+        }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return ((IEnumerable<T>)Mass).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Mass.GetEnumerator();
+        }
+        public T this[int i]
+        {
+            get
+            {
+                if (i < 0 || i >= Count) throw new ArgumentOutOfRangeException();
+                return Mass[i];
+            }
+            set
+            {
+                if (i < 0 || i >= Count) throw new ArgumentOutOfRangeException();
+                Mass[i] = (T)value;
+            }
+        }
+    }
+}
---
+++ Task3.2/Task3.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using DynamicArray;
+
+namespace Task3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mas = new DynamicArray<int>(9);
+            var list = new List<int>{0, 1, 2, 3, 4, 5, 6, 7};
+            var arr = new int[114];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+            DynamicArray<string> mas2 = new DynamicArray<string>();
+            var mas3 = new DynamicArray<int>(list);
+            mas3.Add(100);
+            Stack<int> stack = new Stack<int> ();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4); 
+            stack.Push(5);
+            mas3.AddRange(stack);
+            mas3.AddRange(arr);
+            mas3.Remove(8);
+            mas3.Insert(8, 200);
+            mas3.Insert(128,13);
+            Console.WriteLine(mas3.Length());
+            Console.WriteLine(mas3.Capacity());
+            Print(mas3);
+        }
+        public static void Print<T>(DynamicArray<T> tempArray)
+        {
+            for (int i = 0; i < tempArray.Length(); i++)
+            {
+                Console.Write($"{tempArray[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
